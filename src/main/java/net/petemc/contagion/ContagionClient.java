8dce8f463c9a37a1ab616d0072ec5ae2bde82359
Pete MC-Mods <mcmods.pete@gmail.com>
2024-05-22 09:51:04 +0200

    add Client Class, change LICENSE, small fixes

---
+++ src/main/java/net/petemc/contagion/ContagionClient.java
@@ -0,0 +1,10 @@
+package net.petemc.contagion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ContagionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
