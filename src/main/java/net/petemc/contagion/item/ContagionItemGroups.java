4e0a9c4faaffd300de14b890a627c465a22fb251
Pete MC-Mods <mcmods.pete@gmail.com>
2024-05-28 12:52:19 +0200

    readd Cure potions to Contagion ItemGroup

--- src/main/java/net/petemc/contagion/item/ContagionItemGroups.java
+++ src/main/java/net/petemc/contagion/item/ContagionItemGroups.java
@@ -1,13 +1,14 @@
 package net.petemc.contagion.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.item.*;
-//import net.minecraft.potion.PotionUtil;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.petemc.contagion.Contagion;
+import net.petemc.contagion.potion.ContagionPotions;
 
 public class ContagionItemGroups {
     public static final ItemGroup CONTAGION_GROUP = Registry.register(Registries.ITEM_GROUP,
@@ -17,9 +18,9 @@ public class ContagionItemGroups {
                         entries.add(ContagionItems.CONTAGIOUS_FLESH);
                         entries.add(ContagionItems.HEAT_TREATED_FLESH);
                         entries.add(ContagionItems.GOLD_STREAKED_FLESH);
-                        //entries.add(PotionUtil.setPotion(new ItemStack(Items.LINGERING_POTION), ContagionPotions.CURE_POTION));
-                        //entries.add(PotionUtil.setPotion(new ItemStack(Items.LINGERING_POTION), ContagionPotions.CURE_POTION));
-                        //entries.add(PotionUtil.setPotion(new ItemStack(Items.SPLASH_POTION), ContagionPotions.CURE_POTION));
+                        entries.add(PotionContentsComponent.createStack(Items.POTION, ContagionPotions.CURE_POTION));
+                        entries.add(PotionContentsComponent.createStack(Items.LINGERING_POTION, ContagionPotions.CURE_POTION));
+                        entries.add(PotionContentsComponent.createStack(Items.SPLASH_POTION, ContagionPotions.CURE_POTION));
                     }).build());
 
     public static void registerItemGroups() {
