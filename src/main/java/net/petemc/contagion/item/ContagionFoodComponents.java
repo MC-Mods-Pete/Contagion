0b8d4216ce352d21664ec0139cc53f306239b73f
Pete MC-Mods <mcmods.pete@gmail.com>
2024-05-28 22:57:06 +0200

    small fix to food item

--- src/main/java/net/petemc/contagion/item/ContagionFoodComponents.java
+++ src/main/java/net/petemc/contagion/item/ContagionFoodComponents.java
@@ -10,7 +10,7 @@ public class ContagionFoodComponents {
     public static final FoodComponent CONTAGIOUS_FLESH = new FoodComponent.Builder().hunger(4).saturationModifier(0.1f)
             .statusEffect(new StatusEffectInstance(ContagionEffects.INFECTION, ContagionConfigs.DURATION_INFECTION_TOTAL * 20), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 600, 0), 0.6f).meat().build();
-    public static final FoodComponent HEAT_TREATED_FLESH = new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).build();
+    public static final FoodComponent HEAT_TREATED_FLESH = new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).meat().build();
     public static final FoodComponent GOLD_STREAKED_FLESH = new FoodComponent.Builder().hunger(6).saturationModifier(1.0f)
             .statusEffect(new StatusEffectInstance(ContagionEffects.RESET_INFECTION, 20), 1.0f).alwaysEdible().build();
 }
