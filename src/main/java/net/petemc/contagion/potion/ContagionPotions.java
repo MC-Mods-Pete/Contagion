4f6e0d3ccc47ad871f7b1f3b1105b0cf502e7cef
Pete MC-Mods <mcmods.pete@gmail.com>
2024-06-15 20:03:54 +0200

    - Husks can now also cause the infection - add "long cure potion" - change drop chance for contagious flesh to 5% - add German translation

--- src/main/java/net/petemc/contagion/potion/ContagionPotions.java
+++ src/main/java/net/petemc/contagion/potion/ContagionPotions.java
@@ -2,6 +2,7 @@ package net.petemc.contagion.potion;
 
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.Potions;
 import net.minecraft.registry.Registries;
@@ -15,6 +16,7 @@ import net.petemc.contagion.item.ContagionItems;
 
 public class ContagionPotions {
     public static RegistryEntry<Potion> CURE_POTION;
+    public static RegistryEntry<Potion> LONG_CURE_POTION;
 
     public static RegistryEntry<Potion> registerPotion(String name, StatusEffectInstance statusEffectInstance) {
         return Registry.registerReference(Registries.POTION, Identifier.of(Contagion.MOD_ID, name), new Potion(statusEffectInstance));
@@ -22,12 +24,14 @@ public class ContagionPotions {
 
     public static void registerPotions() {
         CURE_POTION = registerPotion("cure_potion", new StatusEffectInstance(ContagionEffects.IMMUNITY, ContagionConfigs.DURATION_IMMUNITY *20, 0));
+        LONG_CURE_POTION = registerPotion("long_cure_potion", new StatusEffectInstance(ContagionEffects.IMMUNITY, ContagionConfigs.DURATION_IMMUNITY * 3 * 20, 0));
         registerPotionRecipes();
     }
 
     private static void registerPotionRecipes() {
         FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
             builder.registerPotionRecipe(Potions.AWKWARD, ContagionItems.GOLD_STREAKED_FLESH, ContagionPotions.CURE_POTION);
+            builder.registerPotionRecipe(ContagionPotions.CURE_POTION, Items.REDSTONE, ContagionPotions.LONG_CURE_POTION);
         });
     }
 }
