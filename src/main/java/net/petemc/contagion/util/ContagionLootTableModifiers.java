52f7accd5270d89aae509912be0febd838b5c9b9
Pete MC-Mods <mcmods.pete@gmail.com>
2024-08-26 22:42:17 +0200

    take looting into consideration (issue #15), fix problem with death by infection not bypassing armor (issue #16)

--- src/main/java/net/petemc/contagion/util/ContagionLootTableModifiers.java
+++ src/main/java/net/petemc/contagion/util/ContagionLootTableModifiers.java
@@ -2,7 +2,7 @@ package net.petemc.contagion.util;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.condition.RandomChanceWithLootingLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
@@ -29,9 +29,9 @@ public class ContagionLootTableModifiers {
             if(ZOMBIE_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.05f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.075f, 0.025f))
                         .with(ItemEntry.builder(ContagionItems.CONTAGIOUS_FLESH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
             }
@@ -39,9 +39,9 @@ public class ContagionLootTableModifiers {
             if(HUSK_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.05f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.075f, 0.025f))
                         .with(ItemEntry.builder(ContagionItems.CONTAGIOUS_FLESH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
             }
@@ -49,9 +49,9 @@ public class ContagionLootTableModifiers {
             if(DROWNED_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.05f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.075f, 0.025f))
                         .with(ItemEntry.builder(ContagionItems.CONTAGIOUS_FLESH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
             }
@@ -59,9 +59,9 @@ public class ContagionLootTableModifiers {
             if(ZOMBIE_VILLAGER_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.05f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.075f, 0.025f))
                         .with(ItemEntry.builder(ContagionItems.CONTAGIOUS_FLESH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
             }
@@ -69,9 +69,9 @@ public class ContagionLootTableModifiers {
             if(ZOMBIFIED_PIGLIN_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.05f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.075f, 0.025f))
                         .with(ItemEntry.builder(ContagionItems.CONTAGIOUS_FLESH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
             }
@@ -79,9 +79,9 @@ public class ContagionLootTableModifiers {
             if(ZOGLIN_ID.equals(id)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.05f))
+                        .conditionally(RandomChanceWithLootingLootCondition.builder(0.075f, 0.025f))
                         .with(ItemEntry.builder(ContagionItems.CONTAGIOUS_FLESH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
 
                 tableBuilder.pool(poolBuilder.build());
             }
